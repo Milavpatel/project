546495ada8e08bd3be14d41fc163de9165092b5c
Milav <milav.k.p@gmail.com>
2025-07-10 10:21:11 +0000

    Completed with all the functions

--- javascript.js
+++ javascript.js
@@ -13,8 +13,78 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanNum = prompt("Enter your choice: rock, paper or scissors")
-    return humanNum.toLowerCase();
+    let humanInput = prompt("Enter your choice: rock, paper or scissors")
+    humanInput = humanInput.toLowerCase();
+    if(humanInput !== "rock" && humanInput !== "paper" && humanInput !== "scissors"){
+        alert("Please enter valid input");
+        return getHumanChoice();
+    }
+    return humanInput;
 }
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanSelection, computerSelection) {
+    if(humanSelection === "rock" ){
+        if(computerSelection === "rock"){
+           console.log("Tiee");
+        }
+        if(computerSelection === "scissors"){
+            console.log("Congratsss you won !!!");
+            humanScore++;
+        }
+        if(computerSelection === "paper"){
+            console.log("OOPS, you lost ;)")
+            computerScore++;
+        }
+    }
+    if(humanSelection === "paper" ){
+        if(computerSelection === "paper"){
+            console.log("Tiee");
+        }
+        if(computerSelection === "rock"){
+            console.log("Congratsss you won !!!");
+            humanScore++;
+        }
+        if(computerSelection === "scissors"){
+            console.log("OOPS, you lost ;)")
+            computerScore++;
+        }
+    }
+    if(humanSelection === "scissors" ){
+        if(computerSelection === "scissors"){
+            console.log("Tiee");
+        }
+        if(computerSelection === "paper"){
+            console.log("Congratsss you won !!!");
+            humanScore++;
+        }
+        if(computerSelection === "rock"){
+            console.log("OOPS, you lost ;)")
+            computerScore++;
+        }
+    }
+    console.log(`Your choice: ${humanSelection} -- Computer's choice: ${computerSelection}\n`);
 
+}
+function playGame(){
+    for(let i=0; i<5; i++){
+        console.log(`Round ${i+1}`);
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`Score: Human ${humanScore} - Computer: ${computerScore}\n`);
+    }
+
+    if(humanScore > computerScore){
+        return "Congratss You are Winner :)"
+    }
+    if(humanScore < computerScore){
+        return "You lost ;)"
+    }
+    if(humanScore === computerScore){
+        return "Game ended in a Tieee"
+    }
+    console.log("Thank youu");
+    console.log("If you want to play again please refresh the site");
+}
+console.log(playGame())
